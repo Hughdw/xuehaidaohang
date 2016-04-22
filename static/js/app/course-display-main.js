ba26cf00da53f6ac33444e1a52842d7b43078169
Hughdw <whdstyle@gmail.com>
2016-04-22 16:23:43 +0800

    播放页面告一段落

--- static/js/app/course-display-main.js
+++ static/js/app/course-display-main.js
@@ -4,8 +4,12 @@ define(function (require) {
       bindDropdown = require("mod/dropdown");
   $(function() {
 
+    // 绑定 TAG切换事件。
+    $("#sidebar-tabs").on("click", ".sidebar-tab", function(event) {
+      event.preventDefault();//阻止默认事件行为的触发
+      $(this).tab("show");
+    });
 
-    
     // 标题下拉
     bindDropdown.init("#tm-btn",".tit-master");
     // 购物车下拉事件
--- static/js/app/course-list-main.js
+++ static/js/app/course-list-main.js
@@ -14,17 +14,20 @@ define(function (require) {
     var oResetMenu = new ResetMenu(document.getElementById("tab-content"));
     // 默认 触发设置 筛选列表 内容盒子对象的高度
     oResetMenu.render();
+    
     // 窗口重置
     $(window).resize(function() {
       // 触发设置 筛选列表 内容盒子对象的高度
       oResetMenu.render();
     });
+
     // 筛选列表 选项按钮绑定 click
     $("#tab-content").on("click", ".menu-filter-bt", function() {
       // 触发设置 筛选列表 内容盒子对象的高度
       oResetMenu.render();
       // $(this).toggleClass("click");
     });
+
     // 绑定 TAG切换事件。
     $("#menu-tabs").on("click", ".menu-tab", function(event) {
       event.preventDefault();//阻止默认事件行为的触发
