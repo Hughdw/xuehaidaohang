5d0d8080428f130ec476f61f7a8878364b264e44
Hughdw <whdstyle@gmail.com>
2016-05-01 00:15:24 +0800

    手风琴动画完成

--- static/js/app/personal-account-main.js
+++ static/js/app/personal-account-main.js
@@ -2,17 +2,21 @@ define(function (require) {
   var com = require("mod/common");
   var $ = require("jquery");
   $(function() {
-    function tmpFn() {
-      $("#collapse-avatar").collapse("toggle");
-    }
-    // setInterval(tmpFn,4000);
-    // $(".operation").on("click",".collapse-btn",function (event) {
-    //   // event.stopPropagation();//阻止冒泡会影响browersync工具的多窗口同步，暂时注释
-    //   $("#collapse-avatar").collapse("toggle");
-    // });
 
-    // $(".setting").on("click",function(event) {
-    //   $("#collapse-avatar").collapse("toggle");
-    // })
+    $(".operation").on("click",".avatar-btn",function (event) {
+      // event.stopPropagation();//阻止冒泡会影响browersync工具的多窗口同步，暂时注释
+
+      $("#collapse-avatar").collapse({
+        parent:"#accordion"
+      });
+      $("#collapse-avatar").collapse("toggle");
+    });
+
+    $(".operation").on("click",".nickname-btn",function(event) {
+      $("#collapse-nickname").collapse({
+        parent:"#accordion"
+      });
+      $("#collapse-nickname").collapse("toggle");
+    });
   });
 });
