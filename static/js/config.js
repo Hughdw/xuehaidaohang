1a8180e141c26f44363fb55660ed92206e71443b
Hughdw <whdstyle@gmail.com>
2016-05-01 23:43:24 +0800

    日常更新：用户资料样式基本完成，基本交互完成

--- static/js/config.js
+++ static/js/config.js
@@ -6,6 +6,7 @@
 requirejs.config({
     baseUrl: "lib",//设置模块加载的基础路径
     paths: {//指定不在baseUrl根目录的模块路径指向
+        "device": "device/0.2.7/device.min",
         "less": "less/1.7.0/less",
         "jquery": "jquery/1.11.3/jquery.min",
         "bootstrap": "bootstrap/bootstrap-3.3.5/dist/js/bootstrap",
