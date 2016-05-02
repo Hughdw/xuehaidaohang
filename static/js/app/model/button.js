9aef88b87186e29a8a6b1a4008124314925eb50a
Hughdw <whdstyle@gmail.com>
2016-05-02 20:47:46 +0800

    日常更新：未绑定手机和已绑定手机分别展开不同的内容

--- static/js/app/model/button.js
+++ static/js/app/model/button.js
@@ -8,7 +8,7 @@ define(function (require) {
   var fnBtnEvent = function (lk,cl,set) {
     var $Collapse = $(cl);
     var sLinkClassName = lk;
-    var bSetDone = null || set;
+    var bSetDone = set;
     var oBtnTxt = {
       'avatar':['取消','设置','修改'],
       'nickname':['取消','设置','修改'],
@@ -25,13 +25,32 @@ define(function (require) {
       });
       $Collapse.collapse('toggle');
     });
-    $Collapse.on('shown.bs.collapse',function() {
+
+    $Collapse.on('show.bs.collapse',function() {
+      var $Self = $(this);
+      var $Prev = $Self.prev();
       // body...
-      $(this).prev().addClass('al-tit-active').find('.collapse-link').text(oBtnTxt[aBtnType][0]);
+      if (bSetDone === null) {
+        $Prev.addClass('al-tit-active').find('.collapse-link').text(oBtnTxt[aBtnType][0]);
+      }else if (bSetDone) {//已设置
+        $Prev.addClass('al-tit-active').find('.collapse-link').text(oBtnTxt[aBtnType][0]);
+        $Self.find('.collapse-edit').removeClass('hidden');
+      } else {//未设置
+        $Prev.addClass('al-tit-active').find('.collapse-link').text(oBtnTxt[aBtnType][0]);
+        $Self.find('.collapse-default').removeClass('hidden');
+      }
+
     });
-    $Collapse.on('hidden.bs.collapse',function() {
+    $Collapse.on('hide.bs.collapse',function() {
+      var $Self = $(this);
+      var $Prev = $Self.prev();
       // body...
-      $(this).prev().removeClass('al-tit-active').find('.collapse-link').text(oBtnTxt[aBtnType][2]);
+      if (bSetDone || bSetDone === null) {
+        $Prev.removeClass('al-tit-active').find('.collapse-link').text(oBtnTxt[aBtnType][2]);
+      } else {
+        $Prev.removeClass('al-tit-active').find('.collapse-link').text(oBtnTxt[aBtnType][1]);
+      }
+
     });
   };
   return fnBtnEvent;
