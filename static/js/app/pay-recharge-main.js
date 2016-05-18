b6a0ff746d0efb0d75b9e97c2cd6a3075c4d82bf
Hughdw <whdstyle@gmail.com>
2016-05-18 13:31:51 +0800

    准备尝试amd方式

---
+++ static/js/app/pay-recharge-main.js
@@ -0,0 +1,12 @@
+define(function (require) {
+  var $ = require('jquery'),
+      tpldata = require('tpldata'),
+      replaceImgPath = require('mod/replace-img-path');
+  $(function() {
+    // 获取到模版，然后渲染数据
+    // 将选好数据的字符串通过 innerHTML 插入到指定的元素中（#main）
+    document.getElementById('main').innerHTML = template('pay/recharge-main',tpldata);
+    replaceImgPath();
+
+  });
+});
---
+++ static/js/pay-recharge.js
@@ -0,0 +1,3 @@
+requirejs(['static/js/config.js'], function(config) {
+  requirejs(['jquery', 'bootstrap', 'mod/set-min-height', 'app/pay-recharge-main']);
+});
