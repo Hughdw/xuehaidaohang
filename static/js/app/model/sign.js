0cb3432b3e41d97e594005643f815876a0051ff5
Hughdw <whdstyle@gmail.com>
2016-05-19 22:46:25 +0800

    日常更新：增加登录模板；更改模板引入方式为amd；新增忘记密码页面相关文件。

---
+++ static/js/app/model/sign.js
@@ -0,0 +1,15 @@
+define(function(require) {
+  var $ = require('jquery');
+
+  var sign = {
+    switchShowModal : function($Btn,$SignModal) {
+      // var button = $Btn;
+      var showModal = $Btn.data('whatever');
+      var parentModal = $SignModal;
+      var hideModal = $('.modal-content');
+      hideModal.hide();
+      parentModal.find(showModal).show();
+    }
+  };
+  return sign;
+});
--- static/js/app/personal-preferential-main.js
+++ static/js/app/personal-preferential-main.js
@@ -1,13 +1,13 @@
 define(function (require) {
   var $ = require('jquery'),
       tpldata = require('tpldata'),
-      render = require('tpl/personal/preferential-main'),
+      tplPreferentialMain = require('tpl/personal/preferential-main'),
       replaceImgPath = require('mod/replace-img-path');
   $(function() {
     tpldata.sidebar.activeMenu = 4;//设置激活导航
     // 获取到模版，然后渲染数据
     // 将选好数据的字符串通过 innerHTML 插入到指定的元素中（#main）
-    document.getElementById('main').innerHTML = render(tpldata);
+    document.getElementById('main').innerHTML = tplPreferentialMain(tpldata);
     // document.getElementById('main').innerHTML = template('personal/preferential-main',tpldata);
     replaceImgPath();
 
