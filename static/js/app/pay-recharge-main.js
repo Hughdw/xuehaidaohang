9e30c10ff118161fa00c758cb43b3b06f7771fe1
Hughdw <whdstyle@gmail.com>
2016-05-19 11:21:53 +0800

    准备更新.gitgnore文件

--- static/js/app/pay-recharge-main.js
+++ static/js/app/pay-recharge-main.js
@@ -1,13 +1,13 @@
 define(function (require) {
   var $ = require('jquery'),
       tpldata = require('tpldata'),
-      render = require('tpl/pay/recharge-main'),
+      tplRecharte = require('tpl/pay/recharge-main'),
       replaceImgPath = require('mod/replace-img-path');
   $(function() {
 
     // 获取到模版，然后渲染数据
     // 将选好数据的字符串通过 innerHTML 插入到指定的元素中（#main）
-    document.getElementById('main').innerHTML = render();
+    document.getElementById('main').innerHTML = tplRecharte();
     // document.getElementById('main').innerHTML = template('pay/recharge-main',tpldata);
     replaceImgPath();
 
