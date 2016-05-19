6fe0fc8a45fd5fea959aa11c8911fab54d46f983
Hughdw <whdstyle@gmail.com>
2016-05-19 11:21:53 +0800

    准备更新.gitgnore文件

--- static/js/app/index-main.js
+++ static/js/app/index-main.js
@@ -1,6 +1,12 @@
 define(function (require) {
   var $ = require('jquery');
+      tplSign = require('tpl/public/sign-modal'),
+      tplRetrieve = require('tpl/public/password-retrieve-modal');
   $(function() {
+    // 获取到模版，然后渲染数据
+    // 将选好数据的字符串通过 innerHTML 插入到指定的元素中（#main）
+    document.getElementById('sign-modal').innerHTML = tplSign();
+    document.getElementById('password-retrieve-modal').innerHTML = tplRetrieve();
     $('#sign-modal').on('show.bs.modal',function(event) {
       var button = $(event.relatedTarget);
       var showModal = button.data('whatever');
