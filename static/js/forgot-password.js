f559b1e18525c0386c08f45db6f12cf58a2b7199
Hughdw <whdstyle@gmail.com>
2016-06-03 09:56:46 +0800

    日常更新

--- static/js/forgot-password.js
+++ static/js/forgot-password.js
@@ -6,7 +6,7 @@ require(['static/js/config.js'], function(config) {
     'app/controllers/forgot-password-ctrl',
     'app/forgot-password-main'
   ],function() {
-    //
+    // 手动启动 angular 应用
     angular.bootstrap(document,['findPDApp']);
   });
 });
