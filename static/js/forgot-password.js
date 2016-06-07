c64e330b2cf510ec63e79b5cd42d9408061c442c
Hughdw <whdstyle@gmail.com>
2016-06-07 15:19:57 +0800

    存档当前开发分支。 目前状态只有forgot-password相关文件使用AngularJS+requireJS混合方案，其他页面则使用了requireJS+tmod的gulp版生成的模板文件。 配置过程中较为混乱，加上最近AngularJS差不多比较熟悉了，决定全部转向AngularJS。 特此存档。

--- static/js/forgot-password.js
+++ static/js/forgot-password.js
@@ -2,14 +2,11 @@ require(['static/js/config.js'], function(config) {
   require([
     'jquery',
     'bootstrap',
-    'angular',
     'app/route/forgot-password-route',
     'app/controllers/forgot-password-ctrl',
     'app/forgot-password-main'
-  ],function($) {
-    angular.element(document).ready(function() {
-      // 手动启动 angular 应用
-      angular.bootstrap(document,['findPDApp']);
-    });
+  ],function() {
+    // 手动启动 angular 应用
+    angular.bootstrap(document,['findPDApp']);
   });
 });
