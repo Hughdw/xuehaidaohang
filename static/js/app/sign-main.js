889e502c36fea15df07dd81ac9c966a6176cd39a
Hughdw <whdstyle@gmail.com>
2016-06-23 00:07:38 +0800

    日常更新： 1.注册功能基本完成。 2.登录进行一部分。

--- static/js/app/sign-main.js
+++ static/js/app/sign-main.js
@@ -1,5 +1,5 @@
 angular.module('signApp',['ngRoute','formCheck','api'])
-.config(function($routeProvider) {
+.config(function($routeProvider,$httpProvider) {
   $routeProvider
   .when('/login',{
     templateUrl:'./views/sign/login.html'
@@ -10,4 +10,15 @@ angular.module('signApp',['ngRoute','formCheck','api'])
   .otherwise({
     templateUrl:'./views/sign/login.html'
   });
+  $httpProvider.defaults.transformRequest = function(obj){
+    var str = [];
+    for(var p in obj){
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+    return str.join("&");
+  };
+
+  $httpProvider.defaults.headers.post = {
+       'Content-Type': 'application/x-www-form-urlencoded'
+  };
 });
