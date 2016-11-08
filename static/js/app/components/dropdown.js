12c05c7ad9d94263319b23660feec01a7041887b
Hughdw <whdstyle@gmail.com>
2016-11-08 23:15:16 +0800

    1.alert模块的HTML结构提炼成模板。同时更正旧的实现方式。 2.课程播放页面，增加 加入购物车 功能。同时提炼相关HTML模板。 3.课程播放页面，修正 标题显示问题（按钮和下拉列表相同的宽度） 4.更改dropdown模块 收起下拉菜单 实现方式（内模块内部实现） 5.添加到购物车按钮增加loading动画与成功提示。

--- static/js/app/components/dropdown.js
+++ static/js/app/components/dropdown.js
@@ -9,6 +9,17 @@ define(function (require) {
   var _aStoreToggle = [];
   // 存放切换对象的父元素
   var _aStoreToggleParent = [];
+  // 点击空白地方收起标题列表
+  $(document).on('click', function (event) {
+    var jqTarget = $(event.target);
+    for (var i = 0; i < _aStoreToggleParent.length; i++) {
+      // 点击目标不是 激活区域 就还原成下拉框的初始状态
+      if (jqTarget.parents(_aStoreToggleParent[i]).length === 0) {
+        $(_aStoreToggle[i]).addClass('btn-link');
+        $(_aStoreToggle[i]).parents('.btn-group').removeClass('open');
+      }
+    }
+  });
   var oDropdownMenu = {
     // 目标绑定点击事件
     handle: function (toggleBtn, parent) {
@@ -29,23 +40,9 @@ define(function (require) {
           }
         }
       });
-    },
-    dropUp: function () {
-      // 点击空白地方收起标题列表
-      $(document).on('click', function (event) {
-        var jqTarget = $(event.target);
-        for (var i = 0; i < _aStoreToggleParent.length; i++) {
-          // 点击目标不是 激活区域 就还原成下拉框的初始状态
-          if (jqTarget.parents(_aStoreToggleParent[i]).length === 0) {
-            $(_aStoreToggle[i]).addClass('btn-link');
-            $(_aStoreToggle[i]).parents('.btn-group').removeClass('open');
-          }
-        }
-      });
     }
   };
   return {
-    handle: oDropdownMenu.handle,
-    dropUp: oDropdownMenu.dropUp
+    handle: oDropdownMenu.handle
   };
 });
